43cd58720275907c7b617b326089ecf42867953b
Si R <sireisinger@edu.aau.at>
2018-11-22 17:38:20 +0100

    standard dct: implemented

--- at/aau/itec/emmt/jpeg/stud/StandardDCT.java
+++ at/aau/itec/emmt/jpeg/stud/StandardDCT.java
@@ -1,5 +1,6 @@
 package at.aau.itec.emmt.jpeg.stud;
 
+import at.aau.itec.emmt.jpeg.impl.DCTBlock;
 import at.aau.itec.emmt.jpeg.spec.BlockI;
 import at.aau.itec.emmt.jpeg.spec.DCTBlockI;
 import at.aau.itec.emmt.jpeg.spec.DCTI;
@@ -9,7 +10,39 @@ public class StandardDCT implements DCTI {
     @Override
     public DCTBlockI forward(BlockI b) {
 
-        throw new RuntimeException("Not yet implemented :-(");
+        int[][] blockPixels = b.getData();
+        double[][] dctCoeffData = new double[8][8];
+
+        for (int u = 0; u < dctCoeffData.length; u++) {
+            for (int v = 0; v < dctCoeffData[0].length; v++) {
+
+                double sum = 0;
+
+                for (int i = 0; i < blockPixels.length; i++) {
+                    for (int j = 0; j < blockPixels[0].length; j++) {
+
+                        double tmp = (blockPixels[i][j] - 128) *
+                                Math.cos(((2 * i + 1) * u * Math.PI) / 16) *
+                                Math.cos(((2 * j + 1) * v * Math.PI) / 16);
+
+                        sum += tmp;
+                    }
+                }
+
+                dctCoeffData[u][v] = (getConstant(u) * getConstant(v) / 4) * sum;
+            }
+        }
+
+        return new DCTBlock(dctCoeffData);
     }
-    
+
+    private double getConstant(int i) {
+
+        if (i == 0) {
+            return 1 / Math.sqrt(2);
+        }
+
+        return 1;
+    }
+
 }
